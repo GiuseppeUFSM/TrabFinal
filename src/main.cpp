dae6a598dae153a4fbfebb4a5bc7887de5d1c626
GiuseppeUFSM <giuseppe.cervo@acad.ufsm.br>
2021-08-24 15:38:12 -0300

    Versao 1 - Wifi/WEB/TEMP

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,30 @@
 #include <Arduino.h>
 
+
 #include <DHT.h>
 #include <WiFi.h>
 
-#define WIFI_NETWORK = "SUZANA 40 OI FIBRA 2G";
-#define  WIFI_PASSWORD = "10081958";
-#define WIFI_TIMEOUT_MS = 20000;
+
+
+#define WIFI_NETWORK "SUZANA 40 OI FIBRA 2G"
+#define  WIFI_PASSWORD "10081958"
+#define WIFI_TIMEOUT_MS 20000
 
 
 /*
-DHT SENSOR
+HARDWARE
 */
+// DHT22 SENSOR
 DHT my_sensor(5, DHT22);
-
+//LED PINOUT
+int LED = 2;
 
 /*
  * Prototipos das tarefas
  */
 void tarefa_1(void * parameters); //Ler temperatura
 void tarefa_2(void * parameters); //Conectar e Manter WIFI
+void tarefa_3(void * parameters); // Web Server
 /*
 Task Handlers
 */
@@ -28,8 +34,6 @@ Task Handlers
 Globals
 */
 volatile float temperature;
-volatile float temp_sum = 0;
-volatile int temp_ind = 0;
 volatile float temp_med = 0;
 
 /*
@@ -38,17 +42,34 @@ DEFAULT FUNCTIONS
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
-  my_sensor.begin();
+
 
   xTaskCreate(
     tarefa_1,  // nome pro os
     "get_temperature", // nome humano
-    configMINIMAL_STACK_SIZE + 24, // tamanho da pilha
+    1000, // tamanho da pilha
     NULL, // parameters
-    2,  //prioridade
+    3,  //prioridade
     NULL // handle
   );
 
+xTaskCreatePinnedToCore(
+  tarefa_2,
+  "Keep Alive",
+  5000,
+  NULL,
+  2,
+  NULL,
+  CONFIG_ARDUINO_RUNNING_CORE
+);
+xTaskCreate(
+  tarefa_3,
+  "Web_Server",
+  5000,
+  NULL,
+  1,
+  NULL
+);
 
 }
 
@@ -64,37 +85,89 @@ void loop() {
 /* Tarefas de exemplo que usam funcoes para suspender/continuar as tarefas */
 void tarefa_1(void * parameters)
 {
+  my_sensor.begin();
+  volatile float temp_sum = 0;
+  volatile int temp_ind = 0;
 	for(;;)
 	{
 		temperature = my_sensor.readTemperature();
-		med_ind ++;
-		temp_sum = temp_sum + temp_sum;
-		if(med_ind = 6){
+		temp_ind ++;
+		temp_sum = temp_sum + temperature;
+    Serial.print("Temperatura: ");
+    Serial.println(temperature);
+		if(temp_ind == 6){
 			temp_med = temp_sum/temp_ind;
 			temp_ind = 0;
 			temp_sum = 0;
+    Serial.print("Temperatura Media: ");
+    Serial.println(temp_med);
 		} 
-		vTaskDelay(10000);
+		vTaskDelay(2000);
 	}
 }
 
 
 void tarefa_2(void * parameters){
-	fo(;;){
-		if(wiFi.status() == WL_CONNECTED){
+  for(;;){
+		if(WiFi.status() == WL_CONNECTED){
+      Serial.println("[WIFI] Still Connected");
 			vTaskDelay(20000 / portTICK_PERIOD_MS);
 			continue;
 		}
 		Serial.println("WiFI Connecting");
 		WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+
 		unsigned long startAttemptTime = millis(); // contagem p/ timeout
-		while (WiFi.status() != WL_CONNECTED && millis() - startAttmptTime < WIFI_TIMEOUT_MS){
-			Serial.print(".");
-		}
-		If{
-			
-		}
-
+		
+    while (WiFi.status() != WL_CONNECTED && (millis() - startAttemptTime < WIFI_TIMEOUT_MS) ){}
+		if(WiFi.status() != WL_CONNECTED){
+            Serial.println("[WIFI] FAILED");
+            vTaskDelay(21000 / portTICK_PERIOD_MS);
+			  continue;
+        }
+    
+    Serial.println("[WIFI] Connected: " + WiFi.localIP());
 	}
+}
 
+void tarefa_3(void * parameters){
+  WiFiServer server(80);
+  server.begin();
+  for(;;){
+    WiFiClient client = server.available();
+    if (client) { 
+      Serial.println("New Client.");
+      String currentLine = "";
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          if (c == '\n') {
+            if (currentLine.length() == 0) {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println();
+              client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+              client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+              client.println();
+              break;
+            } else {
+              currentLine = "";
+            }
+          } else if (c != '\r') {
+            currentLine += c;
+          }
+          if (currentLine.endsWith("GET /H")) {
+            digitalWrite(LED, HIGH);
+          }
+          if (currentLine.endsWith("GET /L")) {
+            digitalWrite(LED, LOW);
+          }
+        }
+      }
+      client.stop();
+      Serial.println("Client Disconnected.");
+    }
+
+  }
 }
